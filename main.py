b458b497eb8c8dce43da6a02a23530d077472919
andersonyepes1998 <andersonyepesbedoya@gmail.com>
2023-02-18 12:10:47 -0500

    NT2 secrea proyecto y variables en python

---
+++ main.py
@@ -0,0 +1,10 @@
+numeroUno=int (input('Apreciado usuario Digital un número: '))
+numeroDos=int (input('Apreciado usuario Digital un número 2: '))
+
+print(f'El numero uno es {numeroUno}')
+print(f'El numero dos es {numeroDos}')
+
+
+suma = numeroUno + numeroDos
+
+print(f'La suma es {suma}')
