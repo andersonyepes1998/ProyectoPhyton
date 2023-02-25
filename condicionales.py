d02c9bcf0d27b8606987f206ce7757fde7cd964c
anderson <andersonyepesbedoya@gmail.com>
2023-02-24 22:40:17 -0500

    NT2 modificado

---
+++ condicionales.py
@@ -0,0 +1,55 @@
+#PRIMER EJERCICIO
+#numeroMultiple3 = int(input('digitalice un numero: '))
+
+#if(numeroMultiple3 % 3 == 0):
+#    print('El numero ',numeroMultiple3, ' si es multiplo de 3')
+#else:
+#    print('El numero ',numeroMultiple3, 'No es multiplo de 3')
+
+
+
+
+#SEGUNDO EJERCICIO
+#numeroMultiple5 = int(input('digitalice un numero: '))
+
+#if(numeroMultiple5 % 5 == 0):
+#   print('El numero ',numeroMultiple5, ' SI es multiplo de 5')
+#else:
+#   print('El numero ',numeroMultiple5, 'NO es multiplo de 5')
+
+
+#TERCER EJERCICIO
+
+#numeroUno = int(input('digitalice el primer numero: '))
+#numeroDos = int(input('digitalice el segundo numero: '))
+
+#if(numeroUno > numeroDos):
+#    print('El numero',numeroUno, 'es mayor que el numero ',numeroDos)
+#else:
+#    print('El numero',numeroDos, 'es mayor que el numero ',numeroUno)
+
+
+
+
+#CUARTO EJERCICIO
+
+#numeroMayor  = int(input('Cuanta cantidad de dinero tiene Juan: '))
+
+#catidadJuanita = int(numeroMayor/2)
+#cantidadRicardo1 = int(catidadJuanita+numeroMayor)
+#cantidadRicardo2 = int(cantidadRicardo1/2)
+#total = int(catidadJuanita+cantidadRicardo2)
+
+#print('La cantidad de dinero que poseen los tres seria de: ',total)
+
+
+#QUINTO EJERCICIO
+
+ventas = int(input('Cantidad de lincias vendidas: '))
+
+comiciones = int(ventas * 1500000)
+deduciones = int(comiciones*0,5)
+totalDeduciones = int(deduciones+comiciones)
+total = int(3500000 + totalDeduciones)
+
+print('El salario mensual del vendedor es de: ',total)
