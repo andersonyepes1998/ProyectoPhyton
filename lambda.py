4d02f0beb405efd5b6805155c40ffd718389f49c
Luis Miguel <luisariasarias0358@gmail.com>
2023-04-01 13:22:08 -0500

    NTS LAMBDA

---
+++ lambda.py
@@ -0,0 +1,8 @@
+#Declarando una funcion lambda
+
+saludar=lambda nombre: f'hola {nombre}'
+print(saludar('Mathias'))
+
+
+sumar=lambda n1,n2: n1+n2
+print('lA SUMA E SLA SIGUIENTE ',sumar(6,4))
---
+++ tradicional.py
@@ -0,0 +1,13 @@
+#Declaracion de una funcion tradicional....
+
+def saludar(nombre):
+    return f'Hola como estas señor {nombre}'
+    
+resulatdo=saludar('anderson') 
+print(resulatdo)
+
+
+def sumar(n1,n2):
+    return n1+n2
+suma=(6+4)
+print(f'la suma es la siguiente {suma}')
